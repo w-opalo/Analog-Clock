4fd3a6ec9718b94383a75dc3e74f099604c02244
w-opalo <131133776+w-opalo@users.noreply.github.com>
2024-03-15 11:06:28 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+const body = document.querySelector("body"),
+  hourHand = document.querySelector(".hour"),
+  minuteHand = document.querySelector(".minute"),
+  secondHand = document.querySelector(".second");
+
+
+const updateTime = () => {
+
+  let date = new Date(),
+    secToDeg = (date.getSeconds() / 60) * 360,    
+    minToDeg = (date.getMinutes() / 60) * 360,
+    hrToDeg = (date.getHours() / 12) * 360;
+
+
+  secondHand.style.transform = `rotate(${secToDeg}deg)`;
+  minuteHand.style.transform = `rotate(${minToDeg}deg)`;
+  hourHand.style.transform = `rotate(${hrToDeg}deg)`;
+};
+
+
+setInterval(updateTime, 1000);
+
+
+updateTime();
