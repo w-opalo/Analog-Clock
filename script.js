697567ba76f3cd79e19e9ffd9d53200ca4eb8e50
w-opalo <131133776+w-opalo@users.noreply.github.com>
2024-04-12 11:22:21 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,38 +1,23 @@
-const body = document.querySelector("body"),
-  hourHand = document.querySelector(".hour"),
-  minuteHand = document.querySelector(".minute"),
-  secondHand = document.querySelector(".second");
-  
-  // function updateTime(){
-	//   return{
-	// 	  let date = new Date(),
-  //       sec = (date.getSeconds() / 60) * 360,
-  //       min = (date.getMinutes() / 60) * 360,
-  //       hour = (date.getHours() / 60) * 360,
-		  
-	// 	  secondHand.style.transform = `rotate(${sec}deg)`,
-	// 	  minuteHand.style.transform = `rotate(${min}deg)`,
-	// 	  hourHand.style.transform = `rotate(${hour}deg)`
-	//   };
-  // };
-	// setInterval(updateTime, 1000);
-  // updateTime(); 
+const hourHand = document.querySelector(".hour"),
+ minuteHand = document.querySelector(".minute"),
+ secondHand = document.querySelector(".second");
 
-const updateTime = () => {
+function updateTime() {
+  const date = new Date(),
+   sec = (date.getSeconds() / 60) * 360,
+   min = (date.getMinutes() / 60) * 360,
+   hour = (date.getHours() / 12) * 360; // Use 12-hour format
 
-  let date = new Date(),
-    secToDeg = (date.getSeconds() / 60) * 360,    
-    minToDeg = (date.getMinutes() / 60) * 360,
-    hrToDeg = (date.getHours() / 12) * 360;
+  secondHand.style.transform = `rotate(${sec}deg)`;
+  minuteHand.style.transform = `rotate(${min}deg)`;
+  hourHand.style.transform = `rotate(${hour}deg)`;
+}
 
+// Call updateTime initially
+updateTime();
 
-  secondHand.style.transform = `rotate(${secToDeg}deg)`;
-  minuteHand.style.transform = `rotate(${minToDeg}deg)`;
-  hourHand.style.transform = `rotate(${hrToDeg}deg)`;
-};
-
-
+// Update the clock every second
 setInterval(updateTime, 1000);
 
 
-updateTime();
+
